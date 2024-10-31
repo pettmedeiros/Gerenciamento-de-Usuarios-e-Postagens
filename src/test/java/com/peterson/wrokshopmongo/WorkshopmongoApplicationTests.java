2562fd4346de5b1df5799a3a0c7298187c7e2c77
Peterson <pettm20@gmail.com>
2024-10-31 15:29:10 -0300

    Inicio

---
+++ src/test/java/com/peterson/wrokshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterson.wrokshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
