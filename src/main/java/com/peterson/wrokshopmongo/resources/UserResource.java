0ee3f95ab7b15f877f46b63606ee2dc92a700802
Peterson <pettm20@gmail.com>
2024-11-01 14:23:03 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/peterson/wrokshopmongo/resources/UserResource.java
+++ src/main/java/com/peterson/wrokshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.peterson.wrokshopmongo.domain.Post;
 import com.peterson.wrokshopmongo.domain.User;
 import com.peterson.wrokshopmongo.dto.UserDTO;
 import com.peterson.wrokshopmongo.services.UserService;
@@ -64,5 +65,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts()); // retorna a lista de posts do usuário
 	}
 }
