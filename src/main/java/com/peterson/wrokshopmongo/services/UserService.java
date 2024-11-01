137db98168a3ec41216551a0b632b346205c1347
Peterson <pettm20@gmail.com>
2024-11-01 11:23:13 -0300

    Obtendo um usuário por id

--- src/main/java/com/peterson/wrokshopmongo/services/UserService.java
+++ src/main/java/com/peterson/wrokshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.peterson.wrokshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.peterson.wrokshopmongo.domain.User;
 import com.peterson.wrokshopmongo.repository.UserRepository;
+import com.peterson.wrokshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,9 +17,11 @@ public class UserService {
 	private UserRepository repo; 
 	
 	public List<User> findAll(){ // método responsavel por retornar todos os usuários
-		return repo.findAll();
-		
-		 
+		return repo.findAll();		 
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
