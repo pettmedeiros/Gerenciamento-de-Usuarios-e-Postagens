3908fcacf3a682fad5422113734227792926a625
Peterson <pettm20@gmail.com>
2024-11-01 16:41:04 -0300

    Acrescentando comentários aos posts!

--- src/main/java/com/peterson/wrokshopmongo/config/Instantiation.java
+++ src/main/java/com/peterson/wrokshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.peterson.wrokshopmongo.domain.Post;
 import com.peterson.wrokshopmongo.domain.User;
 import com.peterson.wrokshopmongo.dto.AuthorDTO;
+import com.peterson.wrokshopmongo.dto.CommentDTO;
 import com.peterson.wrokshopmongo.repository.PostRepository;
 import com.peterson.wrokshopmongo.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO (maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO (maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(maria));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		PostRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/peterson/wrokshopmongo/domain/Post.java
+++ src/main/java/com/peterson/wrokshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.peterson.wrokshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.peterson.wrokshopmongo.dto.AuthorDTO;
+import com.peterson.wrokshopmongo.dto.CommentDTO;
 
 @Document // indica que a classe é uma representação de um documento MONGODB
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -75,6 +80,14 @@ public class Post implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
