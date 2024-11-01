40c280eaaaaea9666e5c5670d7bf62f8b4aa235a
Peterson <pettm20@gmail.com>
2024-11-01 14:28:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/peterson/wrokshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.peterson.wrokshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.peterson.wrokshopmongo.domain.Post;
+import com.peterson.wrokshopmongo.repository.PostRepository;
+import com.peterson.wrokshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired // mecanismo de injeção de dependencia automática do spring
+	private PostRepository repo; 
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
